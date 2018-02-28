dea0b852e62f77ffd3e73e9ee31645a16d8036de
halona719719 <halona199571@gmail.com>
2018-02-28 18:11:24 -0500

    update zhuoyu

--- src/methods.py
+++ src/methods.py
@@ -2,7 +2,8 @@
 # input type: int(gvkey)
 
 from datetime import datetime
-import pandas as pd
+import pandas
+
 from voluptuous import Schema, Required, MultipleInvalid
 
 
@@ -12,7 +13,7 @@ def get_company_name(**kwargs):
     :return: string (official full name)
     """
     s = Schema({
-        Required('df'): pd.DataFrame,
+        Required('df'): pandas.DataFrame,
         'row_id': int
     })
     try:
@@ -25,13 +26,14 @@ def get_company_name(**kwargs):
         print("error: {} occur while parse with required args".format(e.errors))
 
 
+
 def get_company_tic(**kwargs):
     """
     Example: "MUR"
     :return: string (ticker)
     """
     s = Schema({
-        Required('df'): pd.DataFrame,
+        Required('df'): pandas.DataFrame,
         'row_id': int
     })
     try:
@@ -40,6 +42,7 @@ def get_company_tic(**kwargs):
         ticker = list(df.groupby('ticker')['ticker'].head(1))
         row_id = kwargs['row_id']
         return ticker[row_id]
+
     except MultipleInvalid as e:
         print("error: {} occur while parse with required args".format(e.errors))
 
@@ -59,7 +62,15 @@ def get_today_price(**kwargs):
     assigning today's date.
     :return: dict
     """
-    pass
+    s = Schema({
+        Required('df'): pandas.DataFrame,
+        'row_id': int
+    })
+    try:
+        s(kwargs)  # validate args
+
+    except MultipleInvalid as e:
+        print("error: {} occur while parse with required args".format(e.errors))
 
 
 # --------------------------------------------------------------------
@@ -71,15 +82,20 @@ def datestr2date(**kwargs):
     :param datestr:
     :return: datetime object (date)
     """
+    s=Schema({
+        Required('datestr'):str
+    })
     try:
+        datestr=s['datestr']
         return datetime.strptime(datestr, "%Y%m%d").date()
     except:
         pass
     try:
+        datestr = s['datestr']
         return datetime.strptime(datestr, "%Y-%m-%d").date()
-    except:
+    except MultipleInvalid as e:
         print(
-            "Error: input date string must be a valid date in two of the following formats: 'YYYYMMDD' or 'YYYY-MM-DD'")
+            "Error: input date string must be a valid date in two of the following formats: 'YYYYMMDD' or 'YYYY-MM-DD'".format(e.errors))
         return None
 
 
@@ -92,14 +108,125 @@ def get_history_price(**kwargs):
     return type: dict
     Example: {'open': 15, 'high': 16, 'low':14, 'close':15.5, 'volume': 154640}
     """
-    pass
+    s=Schema({
+        Required('df'):pandas.DataFrame,
+        'date':datetime,
+        'gvkey':int
+    })
+    try:
+        df= s['df']
+        date_=s['date']
+        gvkey=s['gvkey']
+        df=df.loc[df['date']==date_ and df['gvkey']==gvkey]
+        return dict(['open',df['open']],['high',df['high']],['low',df['low']],['close',df['close']],['volume',df['volume']])
+    except MultipleInvalid as e:
+        print("error: input data is not valid".format(e.errors))
+        return None
 
 
+def MA(**kwargs):
+    """
+    Moving Average
+    input type: pandas.DataFrame(df),int(n)
+    n represents # days
+    """
+    s = Schema({
+        Required('df'): pandas.DataFrame,
+        'n': int
+    })
+    try:
+        df=s['df']
+        n=s['n']
+        return df.rolling_mean(df['close'],n)
+    except MultipleInvalid as e:
+        print("error: input data is not valid".format(e.errors))
+def EMA(**kwargs):
+    """
+    ExponentialMoving Average
+    input type: pandas.DataFrame(df),int(n)
+    n represents # days
+    """
+    s = Schema({
+        Required('df'): pandas.DataFrame,
+        'n': int
+    })
+    try:
+        df=s['df']
+        n=s['n']
+        return df.ewma(df['close'],n)
+    except MultipleInvalid as e:
+        print("error: input data is not valid".format(e.errors))
+def ROC(**kwargs):
+    """
+    Rate of Change
+    input type: pandas.DataFrame(df),int(n)
+    n represents # days
+    """
+    s = Schema({
+        Required('df'): pandas.DataFrame,
+        'n': int
+    })
+    try:
+        df=s['df']
+        n=s['n']
+        return df['close'].diff(n-1)/df['close'].shift(n-1)
+    except MultipleInvalid as e:
+        print("error: input data is not valid".format(e.errors))
 def get_history_tech_ind(**kwargs):
     """
     'technical indicator'
-    input type: int(gvkey), dateObject(defined as above),
+    input type: int(gvkey), dateObject(defined as above)
     Example: {'MA':70, "EMA": ..}
     :return: dict
     """
-    pass
+    s=Schema({
+        Required('df'): pandas.DataFrame,
+        'date': datetime,
+        'n':int,
+        'gvkey':int
+    })
+    try:
+        df=s['df']
+        date_=s['date']
+        gvkey=s['gvkey']
+        row_id=df[df['date']==date_ and df['gvkey']==gvkey].index
+        return dict(['MA_7days',MA(df[row_id-6:row_id+1],row_id,7)],['MA_30days',MA(df[row_id-29:row_id+1],row_id,30)],['ROC_52week',ROC(df[row_id+52*7-1:row_id+1],row_id,52*7)])
+    except MultipleInvalid as e:
+        print("error: input is not valid".format(e.errors))
+
+def ahead_behind(**kwargs):
+    """
+    select ahead or behind
+    input type: int(gvkey)
+    """
+    s = Schema({
+        Required('df'): pandas.DataFrame,
+        'date': datetime,
+        'n': int,
+        'gvkey': int
+    })
+    try:
+        df = s['df']
+        date= s['date']
+        gvkey = s['gvkey']
+        return "ahead" if get_history_tech_ind(df,date,gvkey)['ROC_52week'] >0 else "behind"
+    except MultipleInvalid as e:
+        print("error: input data is not valid".format(e.errors))
+def increase_decrease(**kwargs):
+    """
+    select increase or decrease
+    input type:int(gvkey)
+    """
+    s = Schema({
+        Required('df'): pandas.DataFrame,
+        'date': datetime,
+        'n': int,
+        'gvkey': int
+    })
+    try:
+        df = s['df']
+        date= s['date']
+        gvkey = s['gvkey']
+        return "increase" if get_history_tech_ind(gvkey,df,date)['ROC_52week']>0 else "decrease"
+    except MultipleInvalid as e:
+        print("error: input data is not valid".format(e.errors))
